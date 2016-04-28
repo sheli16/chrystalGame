5663a439966b904c702921a7141c0dd2df35aeec
sheli16 <sheli16@live.seminolestate.edu>
2016-04-28 14:13:37 -0400

    function updates

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,8 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
 //function computernumber = Math.floor((Math.random() * 100) + 19;
-var computernumber: function(){
+
+  function computernumber(result) {
     var min = 15;
     var max= 10;
     var randomNumber = Math.floor(Math.random()*(max-min+1)+min;
@@ -11,12 +12,44 @@ var computernumber: function(){
  // });
 
 
- var chrystalNumbers:function(){
+ 
+  function chrystalNumbers(result) {
     var min = 1;
     var max= 5;
     var randChrystal= Math.floor(Math.random()*(max-min+1)+min;
     console.log(randChrystal)
   };
+
+function flipThatCoin(result) {
+  var randomNumber = Math.floor(Math.random() * 2);
+
+  if (randomNumber === 0) {
+    $('#coin-image').html('<img src="http://random-ize.com/coin-flip/us-quarter/us-quarter-front.jpg" />');
+  } else {
+    $('#coin-image').html('<img src="http://random-ize.com/coin-flip/us-quarter/us-quarter-back.jpg" />');
+  }
+
+  if (result === randomNumber) {
+    wins++;
+    $('#win-lose').html('<h2>Winner!</h2>');
+    $('#wins').html('<h3>' + wins + '</h3>');
+  } else {
+    losses++;
+    $('#win-lose').html('<h2>Loser!</h2>');
+    $('#losses').html('<h3>' + losses + '</h3>');
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 var chrystalCount = 0;
@@ -62,7 +95,47 @@ $('#tails').on('click', function() {
 }
 
 
+var headsCount = 0;
+var tailsCount = 0;
+var wins = 0;
+var losses = 0;
 
+$('#heads').on('click', function() {
+  headsCount++;
+  $('#headsChosen').html(headsCount);
+
+  $('#guess').html('<b>Heads</b>');
+  flipThatCoin(0);
+});
+
+
+$('#tails').on('click', function() {
+  tailsCount++;
+  $('#tailsChosen').html(tailsCount);
+
+  $('#guess').html('<b>Tails</b>');
+  flipThatCoin(1);
+});
+
+function flipThatCoin(result) {
+  var randomNumber = Math.floor(Math.random() * 2);
+
+  if (randomNumber === 0) {
+    $('#coin-image').html('<img src="http://random-ize.com/coin-flip/us-quarter/us-quarter-front.jpg" />');
+  } else {
+    $('#coin-image').html('<img src="http://random-ize.com/coin-flip/us-quarter/us-quarter-back.jpg" />');
+  }
+
+  if (result === randomNumber) {
+    wins++;
+    $('#win-lose').html('<h2>Winner!</h2>');
+    $('#wins').html('<h3>' + wins + '</h3>');
+  } else {
+    losses++;
+    $('#win-lose').html('<h2>Loser!</h2>');
+    $('#losses').html('<h3>' + losses + '</h3>');
+  }
+}
 
 
 
