ce00a8a6a2bdcc02c7fbc4a8de524e7587f80a5f
sheli16 <sheli16@live.seminolestate.edu>
2016-04-28 11:28:39 -0400

    js functions

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,22 +2,143 @@
 
 //function computernumber = Math.floor((Math.random() * 100) + 19;
 
-var min = 10;
-var max= 5;
-
-computerNumber: function
-{
+computerNumber: function(){
+    var min = 10;
+    var max= 5;
     var randomNumber = Math.floor(Math.random()*(max-min+1)+min;
-  
     console.log(randomNumber)
-};
+  });
 
-    this.lettersOfTheWord = this.wordInPlay.split('');
-    this.rebuildWordView();
-    this.processUpdateTotalGuesses();
+var chrystalCount = 0;
+var wins = 0;
+var losses = 0;
+
+$('#chrystals').on('click', function() {
+  chrystalCount++;
+  $('#yourNumber').html(chrystalCount);
+
+  $('#yourNumber').html('<H5>Heads</h5>');
+  flipThatCoin(0);
+});
+
+
+$('#tails').on('click', function() {
+  tailsCount++;
+  $('#tailsChosen').html(tailsCount);
+
+  $('#guess').html('<b>Tails</b>');
+  flipThatCoin(1);
+});
+
+function flipThatCoin(result) {
+  var randomNumber = Math.floor(Math.random() * 2);
+
+  if (randomNumber === 0) {
+    $('#coin-image').html('<img src="http://random-ize.com/coin-flip/us-quarter/us-quarter-front.jpg" />');
+  } else {
+    $('#coin-image').html('<img src="http://random-ize.com/coin-flip/us-quarter/us-quarter-back.jpg" />');
+  }
+
+  if (result === randomNumber) {
+    wins++;
+    $('#win-lose').html('<h2>Winner!</h2>');
+    $('#wins').html('<h3>' + wins + '</h3>');
+  } else {
+    losses++;
+    $('#win-lose').html('<h2>Loser!</h2>');
+    $('#losses').html('<h3>' + losses + '</h3>');
+  }
+}
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// We're creating a list of three options for the letters
+  var letters = ['a', 'b', 'c'];
+  // This array will hold what we guess
+  var guessedLetters = [];
+  // This variable will be randomly assigned one of the three letters
+  var letterToGuess = null;
+  // These are what we'll use to count down 
+  var totalGuesses = 9;
+  var guessesLeft = 9;
+  // This is the counter for wins/losses
+  var wins = 0;
+  var losses = 0;
+  // Below we created three functions to updateGuesses, updateGuessesLeft, and updateGuessesSoFar
+  var updateGuessesLeft = function(){
+    // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
+    document.querySelector('#guessesLeft').innerHTML = guessesLeft;
+  };
+  
+  var updateLetterToGuess = function(){
+    // Here we get a random letterToGuess and assign it based on a random generator (only looking at a, b, or c)
+    this.letterToGuess = this.letters[Math.floor(Math.random() * this.letters.length)];
+  };
+  var updateGuessesSoFar = function(){
+    // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
+    document.querySelector('#guessesSoFar').innerHTML = guessedLetters.join(', ');
+  };
+  // Function will be called when we reset everything
+  var reset = function(){
+    totalGuesses = 9;
+    guessesLeft = 9;
+    guessedLetters = [];
+    updateLetterToGuess();
+    updateGuessesLeft();
+    updateGuessesSoFar();
+  }
+  
+  updateLetterToGuess(); //execute on page load
+  updateGuessesLeft();
+  // This function will capture the keyboard clicks. 
+  document.onkeyup = function(event) {
+    // It's going to reduce the guesses by one
+    guessesLeft--;
+    // Lowercase the letter
+    var letter = String.fromCharCode(event.keyCode).toLowerCase();
+    
+    // Then add the guess to the guessedLetters array
+    guessedLetters.push(letter);
+    // Then its going to run the update functions
+    updateGuessesLeft();
+    updateGuessesSoFar();
+    // If we still have guesses left...
+    if (guessesLeft > 0){
+      // We'll check if there's a match.
+      if (letter == letterToGuess){
+        // If there is then we win and we'll update the html to display the win
+        wins++;
+        document.querySelector('#wins').innerHTML = wins;
+        // Then we'll reset the game
+        reset();
+      }
+    // If we are out of guesses...
+    }else if(guessesLeft == 0){
+      // Then we will loss and we'll update the html to display the loss 
+      losses++;
+      document.querySelector('#losses').innerHTML = losses;
+      // Then we'll call the reset. 
+      reset();
 
 
 
